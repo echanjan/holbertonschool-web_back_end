24989d49b07977cfef027faf9e9a4a2a9b2ccac1
echanjan <erickchanjan300696@gmail.com>
2024-02-14 20:15:48 -0500

    update 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Anotaciones en funciones con Python"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Devuelve el valor entero inferior de un número decimal."""
+
+    return math.floor(n)
