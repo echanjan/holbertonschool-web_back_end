3c84b11375172aef4a2827b3b200b9c0b2f4e790
echanjan <erickchanjan300696@gmail.com>
2024-02-14 20:37:15 -0500

    update 5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Anotaciones en funciones con Python"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Devuelve la suma de una lista de números decimales."""
+
+    return sum(input_list)
