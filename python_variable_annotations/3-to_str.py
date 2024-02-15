ba9b2d555794f5af62e7e45bed0140ce83286ffa
echanjan <erickchanjan300696@gmail.com>
2024-02-14 20:23:36 -0500

    update 3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Anotaciones en funciones con Python"""
+
+
+def to_str(n: float) -> str:
+    """Devuelve la representación en cadena de un número decimal."""
+
+    return str(n)
