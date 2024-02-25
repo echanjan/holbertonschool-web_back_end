61c0cf95608f628a82e06509e5d0ca159a4c2f23
echanjan <erickchanjan300696@gmail.com>
2024-02-24 23:02:58 -0500

    update 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""Comprensión asíncrona y generadores asíncronos."""
 
 import asyncio
 import random
 
 
 async def async_generator():
+    """
+    Coroutina que genera valores aleatorios entre 0 y 10
+    de manera asíncrona.
+
+    Cada iteración espera 1 segundo para producir el siguiente número.
+
+    Yields:
+        float: Float entre 0 y 10.
+    """
     for i in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
