fdf1db73311d3f0dd2177c2f69f07804e5d52e44
echanjan <erickchanjan300696@gmail.com>
2024-03-24 19:57:07 -0500

    update 10-car.js

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,13 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const newCar = new Car();
+    Object.setPrototypeOf(newCar, Object.getPrototypeOf(this));
+    return newCar;
+  }
+}
