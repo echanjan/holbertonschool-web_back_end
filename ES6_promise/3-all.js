b14cef9006798d201cb441a7cab39b9df5ff527e
echanjan <erickchanjan300696@gmail.com>
2024-04-12 23:00:25 -0500

    fix eslintrc

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 async function handleProfileSignup() {
   try {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   } catch (error) {
-    console.log("Signup system offline");
+    console.log('Signup system offline');
   }
 }
 
