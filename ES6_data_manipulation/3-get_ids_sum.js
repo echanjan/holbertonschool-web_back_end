8006760e0cddc9ed39bda07475bbb8e1b49a571b
echanjan <erickchanjan300696@gmail.com>
2024-04-21 16:47:24 -0500

    update

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,10 @@
-export default function getStudentIdsSum(getListStudents) {
-  return getListStudents.reduce((acumulador, ids) => acumulador + ids.id, 0);
+export default function getStudentIdsSum(listStudents) {
+  let list = null;
+  if (Array.isArray(listStudents)) {
+    temp = listStudents.reduce(
+      (acumulator, student) => acumulator + student.id,
+      0
+    );
+  }
+  return list;
 }
