328608d314c76abce517eef5d874443eb13bb985
echanjan <erickchanjan300696@gmail.com>
2024-04-21 16:43:34 -0500

    update implemented correctly

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,7 @@
-export default function getStudentsByLocation(getListStudents, city) {
-  return getListStudents.filter((student) => student.location === city);
+export default function getStudentsByLocation(listStudents, location) {
+  let tempArray = "";
+  if (Array.isArray(listStudents) && typeof location === 'string') {
+    tempArray = listStudents.filter((student) => student.location === location);
+  }
+  return tempArray;
 }
