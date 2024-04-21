7dd7b2790eadd3720de3626adb3a1b61a2a1762c
echanjan <erickchanjan300696@gmail.com>
2024-04-21 16:51:53 -0500

    fix sintaxis

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,8 @@
 export default function getStudentIdsSum(listStudents) {
   let list = null;
   if (Array.isArray(listStudents)) {
-    temp = listStudents.reduce(
-      (acumulator, student) => acumulator + student.id,
+    list = listStudents.reduce(
+      (accumulator, student) => accumulator + student.id,
       0
     );
   }
