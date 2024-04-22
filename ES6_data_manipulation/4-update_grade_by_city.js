2ea7c6a7035457963c7c3b84f9d8645185ccab19
echanjan <erickchanjan300696@gmail.com>
2024-04-21 19:12:45 -0500

    fix sintaxis

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,23 +1,13 @@
-export default function updateStudentGradeByCity(
-  ListStudents,
-  city,
-  newGrades
-) {
-  if (
-    !Array.isArray(ListStudentsguit ) ||
-    typeof city !== 'string' ||
-    !Array.isArray(newGrades)
-  ) {
+export default function updateStudentGradeByCity(ListStudents, city, newGrades) {
+  if (!Array.isArray(ListStudents) || typeof city !== 'string' || !Array.isArray(newGrades)) {
     throw new Error('Parámetros no inválidos.');
   }
 
-  return ListStudents.filter((student) => student.location === city).map(
-    (student) => {
-      const newGradeObj = newGrades.find(
-        (grade) => grade.studentId === student.id
-      );
-      const grade = newGradeObj ? newGradeObj.grade : "N/A";
+  return ListStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const newGradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = newGradeObj ? newGradeObj.grade : 'N/A';
       return { ...student, grade };
-    }
-  );
+    });
 }
